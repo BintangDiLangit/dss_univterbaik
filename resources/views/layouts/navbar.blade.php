f97b0d17b88966176b909b3e3b196d95c916edf1
mugiwara <mugiwara@localhost.localdomain>
2022-11-01 16:59:27 +0700

    fixing url

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,20 +1,17 @@
-<nav aria-label="breadcrumb">
+{{-- <nav aria-label="breadcrumb">
     <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
         <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="@yield('link_fp')">@yield('fp')</a>
         </li>
         <li class="breadcrumb-item text-sm text-dark active" aria-current="page">@yield('sp')</li>
     </ol>
     <h6 class="font-weight-bolder mb-0">@yield('sp')</h6>
-</nav>
-<div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+</nav> --}}
+
+<nav aria-label="breadcrumb">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <a href="{{ route('vmatrixkeputusan') }}" class="btn btn-outline-info m-2">SAW</a>
+            <a href="{{ route('wpjumbobot') }}" class="btn btn-outline-info m-2">WP</a>
+        </div>
     </div>
-    <ul class="navbar-nav  justify-content-end">
-        <li class="nav-item d-flex align-items-center">
-            <a href="/" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Log Out</span>
-            </a>
-        </li>
-    </ul>
-</div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,6 @@ use App\Http\Controllers\SkalaController;
 use App\Http\Controllers\ViewMenuController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,16 +29,19 @@ Route::resource('kriteria', KriteriaController::class);
 Route::get('bobot', [BobotController::class, 'index'])->name('bobot.index');
 Route::get('matrix', [MatrixController::class, 'index'])->name('matrix.index');
 
-Route::get('vmatrixkeputusan', [ViewMenuController::class, 'vmatrixkeputusan'])->name('vmatrixkeputusan');
-Route::get('vnormalisasi', [ViewMenuController::class, 'vnormalisasi'])->name('vnormalisasi');
-Route::get('vrangking', [ViewMenuController::class, 'vrangking'])->name('vrangking');
+Route::prefix('saw')->group(function () {
+    Route::get('vmatrixkeputusan', [ViewMenuController::class, 'vmatrixkeputusan'])->name('vmatrixkeputusan');
+    Route::get('vnormalisasi', [ViewMenuController::class, 'vnormalisasi'])->name('vnormalisasi');
+    Route::get('vrangking', [ViewMenuController::class, 'vrangking'])->name('vrangking');
+});
 
-Route::get('wpjumbobot', [ViewMenuController::class, 'wpjumbobot'])->name('wpjumbobot');
-Route::get('wpnilais', [ViewMenuController::class, 'wpnilais'])->name('wpnilais');
-Route::get('wpnilaiv', [ViewMenuController::class, 'wpnilaiv'])->name('wpnilaiv');
-Route::get('wpnormalisasiterbobot', [ViewMenuController::class, 'wpnormalisasiterbobot'])->name('wpnormalisasiterbobot');
-Route::get('wppangkat', [ViewMenuController::class, 'wppangkat'])->name('wppangkat');
-Route::get('wpsums', [ViewMenuController::class, 'wpsums'])->name('wpsums');
-
+Route::prefix('wp')->group(function () {
+    Route::get('wpjumbobot', [ViewMenuController::class, 'wpjumbobot'])->name('wpjumbobot');
+    Route::get('wpnilais', [ViewMenuController::class, 'wpnilais'])->name('wpnilais');
+    Route::get('wpnilaiv', [ViewMenuController::class, 'wpnilaiv'])->name('wpnilaiv');
+    Route::get('wpnormalisasiterbobot', [ViewMenuController::class, 'wpnormalisasiterbobot'])->name('wpnormalisasiterbobot');
+    Route::get('wppangkat', [ViewMenuController::class, 'wppangkat'])->name('wppangkat');
+    Route::get('wpsums', [ViewMenuController::class, 'wpsums'])->name('wpsums');
+});
 
 // });
